cf92b5eea5c2c5637996e8092a7c1c2c80d9d514
Alexander Ou <alexou@femtodata.com>
2026-02-17 22:55:04 -0800

    tests wip

---
+++ tests/database.rs
@@ -0,0 +1,47 @@
+use diesel::{Connection, PgConnection, RunQueryDsl};
+
+pub struct Database {
+    url: String,
+}
+
+impl Database {
+    pub fn new(url: &str) -> Self {
+        Database { url: url.into() }
+    }
+
+    pub fn create(self) -> Self {
+        let (database, postgres_url) = self.split_url();
+        let mut conn = PgConnection::establish(&postgres_url).unwrap();
+        diesel::sql_query(format!(r#"DROP DATABASE IF EXISTS "{}""#, database))
+            .execute(&mut conn)
+            .unwrap();
+        diesel::sql_query(format!(r#"CREATE DATABASE "{}""#, database))
+            .execute(&mut conn)
+            .unwrap();
+        self
+    }
+
+    pub fn conn(&self) -> PgConnection {
+        PgConnection::establish(&self.url)
+            .expect(&format!("failed to establish connection to {}", &self.url))
+    }
+
+    fn split_url(&self) -> (String, String) {
+        let mut split: Vec<&str> = self.url.split('/').collect();
+        let database = split.pop().unwrap();
+
+        // assumes admin database is named 'postgres'
+        let postgres_url = format!("{}/{}", split.join("/"), "postgres");
+        (database.into(), postgres_url)
+    }
+}
+
+impl Drop for Database {
+    fn drop(&mut self) {
+        let (database, postgres_url) = self.split_url();
+        let mut conn = PgConnection::establish(&postgres_url).unwrap();
+        diesel::sql_query(format!(r#"DROP DATABASE IF EXISTS "{}""#, database))
+            .execute(&mut conn)
+            .unwrap();
+    }
+}
--- tests/mod.rs
+++ tests/mod.rs
@@ -8,6 +8,8 @@ use diesel::RunQueryDsl;
 use dotenvy::dotenv;
 use email_address::EmailAddress;
 
+mod database;
+
 #[test]
 fn test_add() {
     assert_eq!(1 + 2, 3);
@@ -17,27 +19,13 @@ fn test_create_user() {
     dotenv().ok();
 
     let db_url = env::var("DATABASE_TEST_URL").expect("DATABASE_TEST_URL env var needed");
+    let db = database::Database::new(&db_url);
 
-    let conn = db::establish_connection(Some(db_url));
+    let conn = db.conn();
 
     let new_user = NewUser {
         username: "test-01",
         email: Some(&EmailAddress::new("test-01@test.com").unwrap()),
         hashed_password: Some(hash_password("blahblahblah".to_string()).unwrap().as_str()),
     };
-}
-
-fn create_test_db(url: &str) {
-    let (database, db_url) = split_url(url.to_owned());
-    let mut conn = db::establish_connection(Some(db_url));
-    diesel::sql_query(format!(r#"CREATE DATABASE "{}""#, database))
-        .execute(&mut conn)
-        .unwrap();
-}
-
-fn split_url(url: String) -> (String, String) {
-    let mut split: Vec<&str> = url.split('/').collect();
-    let database = split.pop().unwrap();
-    let postgres_url = format!("{}/{}", split.join("/"), "postgres");
-    (database.into(), postgres_url)
 }
