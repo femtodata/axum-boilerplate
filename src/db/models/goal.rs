0fe64282919b5d130aabbae0754c5148457e849b
Alexander Ou <alexou@femtodata.com>
2026-02-19 13:26:54 -0800

    goal db

---
+++ src/db/models/goal.rs
@@ -0,0 +1,33 @@
+use diesel::prelude::*;
+
+use crate::db::schema::goals;
+#[derive(Debug, Queryable, Selectable, AsChangeset)]
+#[diesel(table_name = crate::db::schema::goals)]
+#[diesel(check_for_backend(diesel::pg::Pg))]
+#[diesel(treat_none_as_null = true)]
+pub struct Goal {
+    pub id: i32,
+    pub title: String,
+    pub description: String,
+    pub notes: Option<String>,
+    pub user_id: i32,
+}
+
+#[derive(Debug, Insertable)]
+#[diesel(table_name = crate::db::schema::goals)]
+pub struct NewGoal {
+    pub title: String,
+    pub description: String,
+    pub notes: Option<String>,
+    pub user_id: i32,
+}
+
+pub fn create_new_goal(
+    new_goal: &NewGoal,
+    conn: &mut PgConnection,
+) -> Result<Goal, diesel::result::Error> {
+    diesel::insert_into(goals::table)
+        .values(new_goal)
+        .returning(Goal::as_returning())
+        .get_result(conn)
+}
--- src/db/models/mod.rs
+++ src/db/models/mod.rs
@@ -17,25 +17,7 @@ pub use crate::db::models::user::EmailAddress;
 pub use crate::db::models::user::NewUser;
 pub use crate::db::models::user::User;
 
+pub mod goal;
+pub use crate::db::models::goal::Goal;
+pub use crate::db::models::goal::NewGoal;
 // Goal
-
-#[derive(Debug, Queryable, Selectable, AsChangeset)]
-#[diesel(table_name = crate::db::schema::goals)]
-#[diesel(check_for_backend(diesel::pg::Pg))]
-#[diesel(treat_none_as_null = true)]
-pub struct Goal {
-    pub id: i32,
-    pub title: String,
-    pub description: String,
-    pub notes: Option<String>,
-    pub user_id: i32,
-}
-
-#[derive(Debug, Insertable)]
-#[diesel(table_name = crate::db::schema::goals)]
-pub struct NewGoal<'a> {
-    pub title: &'a str,
-    pub description: &'a str,
-    pub notes: Option<&'a str>,
-    pub user_id: i32,
-}
