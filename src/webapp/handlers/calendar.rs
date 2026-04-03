55f7529a08565c306f256f8e0abef06346b2b8fb
Alexander Ou <alexou@femtodata.com>
2026-04-03 16:42:39 -0700

    comment fix

--- src/webapp/handlers/calendar.rs
+++ src/webapp/handlers/calendar.rs
@@ -256,7 +256,7 @@ pub async fn hx_get_calendar_week_content(
     let dates = start_date.iter_days().take(7).collect::<Vec<NaiveDate>>();
 
     let mut context = tera::Context::new();
-    //
+
     // start_date is used for toggling between months and weeks
     let start_date_params = CalendarParams {
         year: start_date.year(),
